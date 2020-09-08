fbb2381db108163b886b5e17a5d8721864fc219c
Christian F <70570052+MarioToast@users.noreply.github.com>
2020-09-08 14:36:17 +0200

    ABC

--- StuffForSchool/StuffForSchool.cpp
+++ StuffForSchool/StuffForSchool.cpp
@@ -2,5 +2,25 @@
 
 int main()
 {
-    
+    char option = ' ';
+    do {
+        std::cout << "Pick A, B or C.\n";
+        std::cin >> option;
+
+        switch (option) {
+        case 'A': case 'a':
+            std::cout << "A for Awesome!\n";
+            break;
+        case 'B': case 'b':
+            std::cout << "B for Badass!\n";
+            break;
+        case 'C': case 'c':
+            std::cout << "C for Cool!\n";
+            break;
+
+        default:
+            std::cout << "Oy! Wrong input!\n";
+            break;
+        }
+    } while (true);
 }
